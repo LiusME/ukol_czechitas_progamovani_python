2db32fb69d43d9159b9cd87c53030fbba26f53db
Lia <kasslovalia@gmail.com>
2024-11-14 18:53:32 +0100

    dodány printy

--- main.py
+++ main.py
@@ -28,6 +28,10 @@ class Estate(Property):
         calculation_estate = self.area *  estate_coefficient * self.locality.locality_coefficient
         return ceil(calculation_estate)
 
+    def __str__(self): 
+        return (f"{self.estate_type.capitalize()} in locality {self.locality.name} " f"({self.locality.locality_coefficient}), {self.area} m², tax: {self.calculate_tax()} Kč.") 
+
+
 class Residence(Property):
     def __init__(self, locality, area, commercial):
         super().__init__(locality, area)
@@ -40,10 +44,21 @@ class Residence(Property):
         else:
             return ceil(calculation_residence)
 
+    def __str__(self):
+        commercial_status = "commercial" if self.commercial else "non-commercial"
+        return (f"Residence in locality {self.locality.name} "
+                f"({self.locality.locality_coefficient}), {self.area} m², "
+                f"{commercial_status}, tax: {self.calculate_tax()} Kč.")
+                
 manetin = Locality("Manětín", 0.8)
 brno = Locality("Brno", 3)
 
 zemedelsky_pozemek = Estate(manetin, 900, 'land')
+print(zemedelsky_pozemek)
 
+dum = Residence(manetin, 120, commercial=False)
+print(dum)
 
+kancelar = Residence(brno, 90, commercial=True)
+print(kancelar)
 
